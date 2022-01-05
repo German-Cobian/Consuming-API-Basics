ff003274ad18ee982ada7d7050ce0353bb205192
German Renato Cobian <germancobian@hotmail.com>
2022-01-05 15:42:29 -0800

    Converte response to json() and console log data

--- apiMain.js
+++ apiMain.js
@@ -3,4 +3,5 @@ const aplication = document.querySelector('.container')
 const url = 'https://jsonplaceholder.typicode.com/users'
 
 fetch(url)
-.then(response => console.log(response))
+.then(response => response.json())
+.then(data => console.log(data))
